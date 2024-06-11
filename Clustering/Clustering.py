98530f712f3ac175ae79f316035676845b5c8d9c
Konrad Matusewicz <konrad.matusewicz@student.htw-berlin.de>
2024-06-11 12:22:27 +0000

    Update Clustering.py

--- Clustering/Clustering.py
+++ Clustering/Clustering.py
@@ -31,19 +31,29 @@ dbscan_clusters = dbscan.fit_predict(X_scaled)
 pca = PCA(n_components=3)
 X_pca = pca.fit_transform(X_scaled)
 
+# Farben und Marker definieren
+colors = ['r', 'g', 'b', 'c', 'm', 'y']
+markers = ['o', '^', 's']
+
 # Plot der in 3. erstellten Cluster mit PCA für jedes K in K-Means
 fig = plt.figure(figsize=(18, 18))
 for i, k in enumerate(k_values):
     ax = fig.add_subplot(2, 2, i + 1, projection='3d')
     for j in range(k):
-        ax.scatter(X_pca[kmeans_clusters[i] == j, 0], X_pca[kmeans_clusters[i] == j, 1], X_pca[kmeans_clusters[i] == j, 2], label=f'Cluster {j + 1}')
+        ax.scatter(X_pca[kmeans_clusters[i] == j, 0], X_pca[kmeans_clusters[i] == j, 1], X_pca[kmeans_clusters[i] == j, 2], color=colors[j % len(colors)], label=f'Cluster {j + 1}')
+    # Plot actual classes
+    for class_idx in range(3):
+        ax.scatter(X_pca[y == class_idx, 0], X_pca[y == class_idx, 1], X_pca[y == class_idx, 2], marker=markers[class_idx], edgecolor='k', facecolor='none', label=iris.target_names[class_idx])
     ax.set_title(f'K-Means Clustering mit PCA (k={k})')
     ax.legend()
 
 # 3D Plot mit PCA für DBScan Cluster
 ax = fig.add_subplot(2, 2, 4, projection='3d')
 for cluster in np.unique(dbscan_clusters):
-    ax.scatter(X_pca[dbscan_clusters == cluster, 0], X_pca[dbscan_clusters == cluster, 1], X_pca[dbscan_clusters == cluster, 2], label=f'Cluster {cluster}')
+    ax.scatter(X_pca[dbscan_clusters == cluster, 0], X_pca[dbscan_clusters == cluster, 1], X_pca[dbscan_clusters == cluster, 2], color=colors[cluster % len(colors)], label=f'Cluster {cluster}')
+
+for class_idx in range(3):
+    ax.scatter(X_pca[y == class_idx, 0], X_pca[y == class_idx, 1], X_pca[y == class_idx, 2], marker=markers[class_idx], edgecolor='k', facecolor='none', label=iris.target_names[class_idx])
 ax.set_title('DBScan Clustering mit PCA')
 ax.legend()
 
@@ -53,9 +63,7 @@ plt.show()
 # Scatter-Diagramm für K-Means Cluster und Klassen
 plt.figure(figsize=(18, 18))
 
-# Farben und Marker definieren
-colors = ['r', 'g', 'b', 'c', 'm', 'y']
-markers = ['o', '^', 's']
+
 
 for i, k in enumerate(k_values):
     plt.subplot(2, 2, i + 1)
@@ -67,7 +75,6 @@ for i, k in enumerate(k_values):
     plt.xlabel('Sepal Length')
     plt.ylabel('Sepal Width')
     plt.legend()
-
 plt.tight_layout()
 plt.show()
 
@@ -84,3 +91,9 @@ plt.legend()
 
 plt.tight_layout()
 plt.show()
+
+#Bewertung des Clustering Algorithmen
+#K=2: Gute Trennung von der Setosa-Klasse. Allerdings leichte vermischung von Versicolor und Virginica
+#K=3: Gute Trennung aller drei Klassen. Die Cluster entsprechend überwiegend den Klassen.
+#k=4: Klassen werden in mehrere Cluster unterteilt. Dies ist eher weniger sinnvoll.
+#DBScan: Dichte Clusterbildung, allerdings nicht eindeutig getrennt wie bei K=3
